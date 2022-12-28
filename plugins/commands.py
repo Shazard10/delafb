f941b5a906330e789e3ac9051ffff1db25812dc3
Shanid TG <109721076+Shazard10@users.noreply.github.com>
2022-12-28 16:09:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
-                InlineKeyboardButton('🤖 Updates', url='https://t.me/WPM_OTT')
+                InlineKeyboardButton('🤖 Updates', url='https://t.me/new_movies_group_2021')
             ],
             [
                 InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
